f2807dcbd16cf257888c854a87c33b9dee22085e
Kirill <107070209+iSpotium@users.noreply.github.com>
2022-07-28 04:05:48 +0300

    Add files via upload

---
+++ src/by/oop/hometask/cusproject/CustomerLogic.java
@@ -0,0 +1,50 @@
+package by.oop.hometask.cusproject;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class CustomerLogic {
+////////////////////////////////////////////////////////////
+	public boolean searchCards(Customer cus) {
+
+		int cardnum = cus.getCreditCardNumber();
+
+		if ((100 <= cardnum) && (cardnum <= 116)) {
+			return true;
+		}
+		return false;
+	}
+
+////////////////////////////////////////////////////////////
+	public List<Customer> searchCardList(List<Customer> cus) {
+		List<Customer> cardlis = new ArrayList<Customer>();
+		for (Customer i : cus) {
+			if (searchCards(i)) {
+				cardlis.add(i);
+			}
+		}
+		return cardlis;
+	}
+
+////////////////////////////////////////////////////////////
+	public void printSearchCards(List<Customer> cus) {
+		System.out.println("List of buyers whose credit card number is in the given interval");
+		System.out.println("-------------------------------------------------------");
+
+		for (Customer i : cus) {
+			System.out.println(
+					"Surname: " + i.getSurname() + ", Name: " + i.getName() + ", Patronymic: " + i.getPatronymic());
+		}
+	}
+
+
+///////////////////////////////////////////////////////////
+	public void printList(List<Customer> cus) {
+		for (Customer i : cus) {
+			System.out.println("Surname: " + i.getSurname() + ", Name: " + i.getName() + ", Patronymic: " + i.getPatronymic());
+		}
+	}
+///////////////////////////////////////////////////////////
+}
